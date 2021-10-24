31a2960facf34ea44a88cbdba0c741f05104ec50
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-10-24 23:06:43 +0400

    Create Ex4.UntidyCode.cpp

---
+++ HomeWork/Ex4.UntidyCode.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    int number = 6;
+    cout << "This is untidy code."<<endl;
+    cout << "I'm surprised it works." << endl;
+    cout << "It has " << number << " lines of code-" << endl;
+    cout << "each more hideous than the last." << endl;
+    cout << "You probably should add some line breaks in the text too" << endl; // removed all the extra spaces and added a endl
+    return 0;
+}
