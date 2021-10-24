937a9cc263711450e4967c63718e5b4219ea3e63
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-10-24 23:06:21 +0400

    Create Ex2.DataTypes.cpp

---
+++ HomeWork/Ex2.DataTypes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int firstNumber = 1; // using integer 
+    cout << "firstNumber = " << firstNumber ;
+    bool iCanCode = true; // using boolean
+    cout << "iCanCode = " << iCanCode << endl;
+    char hopefulGrade = 'a'; // using character
+    cout << "hopefulGrade = " << hopefulGrade << endl;
+    double myDecimal = 1.0; // using double
+    cout << "myDecimal = " << myDecimal << endl;
+    string minimalSentance = "y"; // using string
+    cout << "minimalSentance = " << minimalSentance << endl;
+    int keyMash = 13213123; // using int
+    cout << "keyMash = " << keyMash << endl;
+    float mysteryDataType = 5.6f; // using float
+    cout << "mysteryDataType = " << mysteryDataType << endl;
+    return 0;
+}
