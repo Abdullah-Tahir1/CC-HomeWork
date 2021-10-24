677fc1da62f1de696b6f2fa7b50ece9cd1f808f5
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-10-24 23:06:56 +0400

    Create Ex7.BiographyPart1.cpp

---
+++ HomeWork/Ex7.BiographyPart1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string name = "Abdullah"; // Assigned String as Data type
+	int age = 19; // Assigned integer as Data type
+	string hometown = "Karachi"; // Assigned String as Data type
+	cout << "Name: " << name << endl; // using endl to print every statement on a seperate line
+	cout << "Age:  " << age << endl;
+	cout << "Hometown: " << hometown << endl;
+	return 0;
+}
