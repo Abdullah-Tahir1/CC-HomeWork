d998aafc9e75ca0488cc02d5d7791b8ef2c7995d
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-10-24 23:06:07 +0400

    Create Ex1.Introduction.cpp

---
+++ HomeWork/Ex1.Introduction.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Name: Abdullah Tahir" << endl; // Printing out name using cout command
+	cout << "Age: 19" << endl; // Printing out age using cout command
+	cout << "Current Address: Ajman, UAE" << endl; // Printing out Current Address using cout command
+	cout << "Permanent Address: Pakistan" << endl; // Printing out Permanent Address using cout command
+	return 0; 
+}
