cc4d3ddbc29f51a8eab27c69918877209354609b
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-10-24 23:07:15 +0400

    Create Ex9.TemperaturePart1.cpp

---
+++ HomeWork/Ex9.TemperaturePart1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float celsius = 37; // assigning 37 value to celsius with float datatype
+	cout << celsius << " celsius is " << (celsius * 1.8000) + 32 << " in Fahrenheit.\n"; // F = c*1.8000 + 32
+	return 0;
+}
