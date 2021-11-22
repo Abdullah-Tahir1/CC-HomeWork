f5baafd029666fda6c37decbf91d2a0f2fc99ad1
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-11-22 09:31:33 +0400

    Create Reverse9TimesTable.cpp

---
+++ Loops/Reverse9TimesTable.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+int main() {
+	int num = 108; // starting from 108
+	while (num > 8) // using while loop
+	{
+		cout << num << endl;
+		num = num - 9; // decrement of 9 every time the loop runs
+	}
+		return 0;
+}
