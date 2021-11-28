e5e5ad6364d8ef2da595614a03628da8d401cd22
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-11-28 22:39:46 +0400

    Create TimeTravel.cpp

---
+++ Array/TimeTravel.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string a[12] = { "jan","feb","mar","apr","may", "jun", "jul", "aug", "sept", "oct", "nov", "dec", };
+	for (int i = 0; i < 12; i++)
+	{
+		cout << a[i] << endl;
+	}
+	return 0;
+}
