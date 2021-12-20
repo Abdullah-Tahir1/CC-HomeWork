bc664b55b13487592ae5be5184afc0ef2267f0b9
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-12-20 14:01:16 +0400

    Update SevenStarsSevenLines.cpp

--- Loops/SevenStarsSevenLines.cpp
+++ Loops/SevenStarsSevenLines.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-	for (int i = 1; i < 8; i++)
+	for (int i = 1; i < 8; i++) // Using nested for loop to print the disred output
 	{
 		for (int j = 7; j > 0; j--)
 		{
