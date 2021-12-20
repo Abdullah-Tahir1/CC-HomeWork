c58279c3274f9ca2861ba5300f36cd0a6902bb4d
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-12-20 13:59:31 +0400

    Update RemainPositive.cpp

--- Loops/RemainPositive.cpp
+++ Loops/RemainPositive.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
 	float myNum = 20;
-	while (myNum > 0)
+	while (myNum > 0) // using while to print from 20 to 0.5 while keep subtracting 0.5
 	{
 		cout << myNum << endl;
 		myNum -= 0.5;
