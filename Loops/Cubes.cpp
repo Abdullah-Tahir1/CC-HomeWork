13f36f1b6f24b226c329398cf86ce54ebc347b27
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-12-20 14:01:01 +0400

    Update Cubes.cpp

--- Loops/Cubes.cpp
+++ Loops/Cubes.cpp
@@ -5,7 +5,7 @@ int main()
 	int n,c=1;
 	cout << "Enter a number: ";
 	cin >> n;
-	while (!cin.fail() && c <= n)
+	while (!cin.fail() && c <= n) // checking if the user entered the entered input
 	{
 		cout << "Number is " << c << " and the cube is " << c * c * c << endl;
 		c++;
