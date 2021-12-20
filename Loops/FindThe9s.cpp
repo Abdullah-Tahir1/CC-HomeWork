442a4e2a8e4c6da2718a8684bef362a9ee0442d4
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-12-20 13:59:12 +0400

    Update FindThe9s.cpp

--- Loops/FindThe9s.cpp
+++ Loops/FindThe9s.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-	int sum = 0;
-	for (int i = 100; i < 201; i++)
+	int sum = 0; // Initilizing an int variable "sum" with 0 value
+	for (int i = 100; i < 201; i++) // Using for loop to cycle through the number 0 - 200
 	{
-		if (i % 9 == 0)
+		if (i % 9 == 0) // using if statement and putting the condition such that if the number is divisible by 9, it'll add it to sum
 		{
 			sum += i;
 		}
 	}
-	cout << "The sum of every 9, from 100 to 200 is: " << sum << endl;
+	cout << "The sum of every 9, from 100 to 200 is: " << sum << endl; // Printing out the anwer
 	return 0;
 }
