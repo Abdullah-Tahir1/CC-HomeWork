254fa54848a9c9a4115321bd0bb885ab8e09c8c8
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-12-20 13:59:09 +0400

    Update IterateThroughAWorld.cpp

--- Loops/IterateThroughAWorld.cpp
+++ Loops/IterateThroughAWorld.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-	string myWord = "Abdullah Is Shareef";
-	for (int i = 0; i < myWord.length(); i++)
+	string myWord = "Hello World"; // Intializing an array
+	for (int i = 0; i < myWord.length(); i++) // Using for loop to print the string letter by letter
 	{
 		cout << myWord.at(i) << endl;
 	}
