d28bce7793ac97e59e44f3d2398afcd6876be431
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-12-20 13:59:20 +0400

    Update OddOrEven.cpp

--- Loops/OddOrEven.cpp
+++ Loops/OddOrEven.cpp
@@ -3,19 +3,15 @@ using namespace std;
 int main()
 {
 	int x = 20;
-	for (x = 20; x < 25; x++)
+	for (x = 20; x < 25; x++) // using for loop to cycle from 20 to 25
 	{
-		if (x % 2 == 0)
+		if (x % 2 == 0) // if the number's remainder is 0 then its even
 		{
 			cout << x << " - even" << endl;
 		}
-		else if (x % 2 == 1)
+		else if (x % 2 == 1)// if the number's remainder is 1 then its odd
 		{
 			cout << x << " - odd" << endl;
-		}
-		else
-		{
-			cout << "Invalid" << endl;
 		}
 	}
 	return 0;
