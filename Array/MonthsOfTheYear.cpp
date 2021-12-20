38e17a146b4b46c51f718b4547ed8a36b3e4d62f
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-12-20 14:00:46 +0400

    Update MonthsOfTheYear.cpp

--- Array/MonthsOfTheYear.cpp
+++ Array/MonthsOfTheYear.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-	string a[12] = {"jan","feb","mar","apr","may", "jun", "jul", "aug", "sept", "oct", "nov", "dec",};
+	string a[12] = {"jan","feb","mar","apr","may", "jun", "jul", "aug", "sept", "oct", "nov", "dec",} // Declaring array with string data type
 	return 0;
 }
