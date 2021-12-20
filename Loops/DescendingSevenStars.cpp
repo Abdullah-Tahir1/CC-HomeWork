55fa4e0dca8fb532f7aaf1f1953dfb7d78f36b12
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-12-20 14:01:08 +0400

    Update DescendingSevenStars.cpp

--- Loops/DescendingSevenStars.cpp
+++ Loops/DescendingSevenStars.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-	for (int i = 7; i > 0; i--)
+	for (int i = 7; i > 0; i--) // Using nested for loop to print the desired output
 	{
 		for (int j = i; j > 0; j--)
 		{
