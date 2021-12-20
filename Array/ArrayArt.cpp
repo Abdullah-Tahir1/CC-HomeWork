0576f5948bf00385b4c7ba165164ee30274ddcfa
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-12-20 13:58:49 +0400

    Update ArrayArt.cpp

--- Array/ArrayArt.cpp
+++ Array/ArrayArt.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main()
 {
-	char a[5][5] = { {'-','-','-','-','-'},{'-','O','-','O','-'},{'-','@','@','@','-'},{'-','^','^','^','-'},{'-','v','v','v','-'} };
+	char a[5][5] = { {'-','-','-','-','-'},{'-','O','-','O','-'},{'-','@','@','@','-'},{'-','^','^','^','-'},{'-','v','v','v','-'} }; // declaring and initializing a 2d array
 	for (int i = 0; i < 5; i++) // rows
 	{
 		for (int j = 0; j < 5; j++) // columns
 		{
-			cout << a[i][j];
+			cout << a[i][j]; // printing out the 2d array
 		}
 		cout << endl;
 	}
