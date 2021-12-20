9b45e314a1172a3b4b6acf87361e075dd91a6329
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-12-20 13:58:57 +0400

    Update TimeTravel.cpp

--- Array/TimeTravel.cpp
+++ Array/TimeTravel.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-	string a[12] = { "jan","feb","mar","apr","may", "jun", "jul", "aug", "sept", "oct", "nov", "dec", };
-	for (int i = 0; i < 12; i++)
+	string a[12] = { "jan","feb","mar","apr","may", "jun", "jul", "aug", "sept", "oct", "nov", "dec", }; // Declaring array with string data type
+	for (int i = 0; i < 12; i++) // running a for loop to print out all the values in the array
 	{
 		cout << a[i] << endl;
 	}
