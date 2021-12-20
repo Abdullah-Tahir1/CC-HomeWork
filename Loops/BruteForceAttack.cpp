2a0a143fa8fc3fd0ed27b4acc5b6d18699bfacac
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-12-20 13:59:06 +0400

    Update BruteForceAttack.cpp

--- Loops/BruteForceAttack.cpp
+++ Loops/BruteForceAttack.cpp
@@ -5,7 +5,7 @@ int main()
 	string u, p = "246";
 	cout << "Enter Your Password: ";
 	cin >> u;
-	while (u != p && !cin.fail())
+	while (u != p && !cin.fail()) // using while loop to repeat the loop tll user enters the correct passsword
 	{
 		cout << "Try Again.\nEnter Your Password:";
 		cin >> u;
