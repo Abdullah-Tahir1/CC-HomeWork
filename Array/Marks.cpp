1957b058f58182285afc967c21c9637dba75e36d
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-12-20 13:58:53 +0400

    Update Marks.cpp

--- Array/Marks.cpp
+++ Array/Marks.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 int main()
 {
-	int a[5], sum = 0;
+	int a[5], sum = 0;  // Declaring an array without initializing it
 	cout << "Enter the marks." << endl;
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 5; i++) // running a loop to get input for the array from the user
 	{
 		cin >> a[i];
-		while (cin.fail())
+		while (cin.fail()) // while loop for invalid inputs
 		{
 			cin.clear();
 			cin.ignore(1000, '\n');
 			cout << "Invalid number\nTry again: ";
 			cin >> a[i];
 		}
-		sum += a[i];
+		sum += a[i]; // adding all the values
 	}
-	cout << "The average is : " << sum / 5 << endl;
+	cout << "The average is : " << sum / 5 << endl; // dividing the sum with 5 to get the avg
 	return 0;
 }
