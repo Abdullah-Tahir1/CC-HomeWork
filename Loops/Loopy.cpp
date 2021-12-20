e4f80dec391207d5b30ba564b300a29d765e47eb
Abdullah Tahir <abdullahtahir9802@gmail.com>
2021-12-20 13:59:16 +0400

    Update Loopy.cpp

--- Loops/Loopy.cpp
+++ Loops/Loopy.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
 int main() {
-    int myInt;
-    int counter = 0;
-    cout << "Enter a number: ";
-    cin >> myInt;
-    while (counter <= myInt)
-    {
-        cout << counter << endl;
-        counter++;
-    }
-    return 0;
+	int myInt;
+	int counter = 0;
+	cout << "Enter a number : ";
+	cin >> myInt;
+	do // using do while to print from 0 to user's input
+	{
+		cout << counter << endl;
+		counter++;
+	} while (counter <= myInt); // putting the condition such tht it'll only print till user's givien number
+	return 0;
 }
